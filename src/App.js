7864c72a0b1d977bdefe74436defa3d553d74876
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-04 03:04:47 -0400

    Initial layout and folder structure is done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CssBaseline, Grid } from '@material-ui/core'
 
 import Header from './components/Header/Header'
 import List from './components/List/List'
+import Map from './components/Map/Map'
 
 const App = () => {
     return (
@@ -13,8 +14,8 @@ const App = () => {
                 <Grid item xs={12} md={4}>
                     <List />
                 </Grid>
-                <Grid item xs={12} md={4}>
-                    
+                <Grid item xs={12} md={8}>
+                    <Map />
                 </Grid>
             </Grid>
         </>
