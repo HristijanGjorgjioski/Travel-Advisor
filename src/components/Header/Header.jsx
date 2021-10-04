877617f053658583a4596cd31c5fb97c319837ec
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-04 03:17:01 -0400

    Working on the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from '@material-ui/core'
+import { Autocomplete } from '@react-google-maps/api'
+import SearchIcon from '@material-ui/icons'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore New Places
+                    </Typography>
+                    <Autocomplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Map = () => {
     return (
         <div>
-            
+            Map
         </div>
     )
 }
