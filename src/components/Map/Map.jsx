6ecb2e0788790dd8f9e33b620078089b84b20ea7
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-05 04:12:48 -0400

    Creating styles for Map comp.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
-import LocationOutLinedIcon from '@material-ui/icons/LocationOutlined'
+import LocationOutLinedIcon from '@material-ui/icons/LocationOnOutlined'
 import Rating from '@material-ui/lab'
 
-// import useStyles from './styles'
+import useStyles from './styles'
 
 const Map = () => {
+    const classes = useStyles()
+
     return (
         <div>
             Map
