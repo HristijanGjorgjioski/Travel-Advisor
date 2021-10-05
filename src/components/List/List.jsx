3a0d04a544db7e7d03b21a2d816778f1ae7deb7d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-05 12:33:34 -0400

    Working on List comp.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Typography, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
 
 import useStyles from './styles'
 
 const List = () => {
     const classes = useStyles()
+    const [type, setType] = useState('restaurants')
 
     return (
-        <div>
-            
+        <div className={classes.container}>
+            <Typography variant="h4">Hotels, Restaurants & Attractions around you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={''} onChange={''}>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
         </div>
     )
 }
