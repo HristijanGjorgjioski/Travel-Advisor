f0ce06282f1ba2cf37a926ff0ac5954efcbf92b6
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-06 10:56:01 -0400

    Time to test if api request code is ok

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,27 @@
 import axios from 'axios'
 
-const getPlacesData = async () => {
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+
+const options = {
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+  },
+  headers: {
+    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    'x-rapidapi-key': 'a0d5aa861cmsh16f88ad471beb02p158c89jsn2ba70d20aad1'
+  }
+};
+
+export const getPlacesData = async () => {
     try {
-        const response = await axios.get()
+        const { data: { data } } = await axios.get(URL, options)
+
+        return data
     } catch (error) {
-        
+        console.log(error)
     }
 }
