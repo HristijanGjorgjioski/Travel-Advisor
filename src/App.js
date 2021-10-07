8df92bc68bc0bd8c501eec5584e206417fe7b8e8
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-07 03:21:34 -0400

    Started to work on PlaceDetails component

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
     }, []);
 
     useEffect(() => {
-        getPlacesData(bounds.sw, bounds.ne)
+        getPlacesData(bounds?.sw, bounds?.ne)
             .then((data) => {
                 setPlaces(data)
             })
