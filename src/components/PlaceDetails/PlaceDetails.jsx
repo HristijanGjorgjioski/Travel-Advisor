05a51f6441261c7de16b243e3c3cf39ad865fc34
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-07 03:41:41 -0400

    Successfuly displaying restaurants info

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -7,11 +7,20 @@ import { Rating } from '@material-ui/lab'
 import useStyles from './styles'
 
 const PlaceDetails = ({ place }) => {
-    console.log(place)
+    const classes = useStyles()
+
+
     return (
-        <div>
-            {place.name}
-        </div>
+        <Card elevation={6}>
+            <CardMedia
+                style={{ height: 350 }}
+                image={place.photo ? place.photo.images.large.url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Barbieri_-_ViaSophia25668.jpg/1200px-Barbieri_-_ViaSophia25668.jpg'}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">{place.name}</Typography>
+            </CardContent>
+        </Card>
     )
 }
 
