ba3fbf677c3e30aef5e24a5af8bdbf01dbc6dc13
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-07 03:49:50 -0400

    WOrking on Restaurant card layout

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -19,6 +19,21 @@ const PlaceDetails = ({ place }) => {
             />
             <CardContent>
                 <Typography gutterBottom variant="h5">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+                {place?.awards?.map((award, i) => (
+                    <Box my={1} key={i} display="flex" justifyContent="space-between">
+                        <img src={award.images.small} alt="award" />
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                
             </CardContent>
         </Card>
     )
