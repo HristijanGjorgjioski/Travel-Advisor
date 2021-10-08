f9a61465b34ff065cc5b3705948e173a666df24c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-08 04:47:16 -0400

    Successfuly filtering hotels and restaurants by rating

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Map from './components/Map/Map'
 
 const App = () => {
     const [places, setPlaces] = useState([])
+    const [filteredPlaces, setFilteredPlaces] = useState([])
+
     const [bounds, setBounds] = useState({})
     const [coordinates, setCoordinates] = useState({})
     const [childClicked, setChildClicked] = useState(null)
@@ -23,14 +25,20 @@ const App = () => {
     }, []);
 
     useEffect(() => {
+        const filteredPlaces = places?.filter((place) => place.rating > rating)
+
+        setFilteredPlaces(filteredPlaces)
+    }, [rating])
+
+    useEffect(() => {
         setIsLoading(true)
-        getPlacesData(bounds.sw, bounds.ne)
+        getPlacesData(type, bounds.sw, bounds.ne)
             .then((data) => {
                 setPlaces(data)
-
+                setFilteredPlaces([])
                 setIsLoading(false)
             })
-    }, [coordinates, bounds])
+    }, [type, coordinates, bounds])
 
     return (
         <>
@@ -39,7 +47,7 @@ const App = () => {
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
                     <List 
-                        places={places}
+                        places={filteredPlaces.length ? filteredPlaces : places}
                         childClicked={childClicked}
                         isLoading={isLoading}  
                         type={type}
@@ -53,7 +61,7 @@ const App = () => {
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
-                        places={places}
+                        places={filteredPlaces.length ? filteredPlaces : places}
                         setChildClicked={setChildClicked}
                     />
                 </Grid>
