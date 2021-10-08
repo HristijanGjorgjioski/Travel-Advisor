00fd285f564d914e370a084a88ab221a355a8f29
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-10-08 05:58:25 -0400

    The app is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,33 +10,20 @@ const Header = ({ setCoordinates }) => {
     const [autocomplete, setAutocomplete] = useState(null)
 
     const onLoad = (autoC) => setAutocomplete(autoC)
-    
+    console.log(autocomplete)
     const onPlaceChanged = () => {
         const lat = autocomplete.getPlace().geometry.location.lat()
         const lng = autocomplete.getPlace().geometry.location.lng()
     
         setCoordinates({ lat, lng })
     }
-
+    
     return (
         <AppBar position="static">
             <Toolbar className={classes.toolbar}>
                 <Typography variant="h5" className={classes.title}>
                     Travel Advisor
                 </Typography>
-                <Box display="flex">
-                    <Typography variant="h6" className={classes.title}>
-                        Explore New Places
-                    </Typography>
-                    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-                        </div>
-                    </Autocomplete>
-                </Box>
             </Toolbar>
         </AppBar>
     )
